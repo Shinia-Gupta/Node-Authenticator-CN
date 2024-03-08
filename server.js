01cbe2da7e6e2774837a40f6325fce1b97b87a7f
Shinia <113818197+Shinia-Gupta@users.noreply.github.com>
2024-03-08 09:19:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import { connectDB } from './config/connection.js';
 import dotenv from 'dotenv';
 dotenv.config();
 app.listen(process.env.PORT,process.env.HOSTNAME,()=>{
-    console.log('server is listening at port 8001');
+    console.log(`server is listening at port ${process.env.PORT}`);
     connectDB();
 })
