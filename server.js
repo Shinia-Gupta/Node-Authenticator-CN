a8dea57784d181162f081981334a5a40a901953d
Shinia <113818197+Shinia-Gupta@users.noreply.github.com>
2024-03-08 08:50:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import app from './index.js';
 import { connectDB } from './config/connection.js';
-app.listen(8001,()=>{
+const hostname="0.0.0.0";
+app.listen(8001,hostname,()=>{
     console.log('server is listening at port 8001');
     connectDB();
 })
